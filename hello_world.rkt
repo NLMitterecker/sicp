277a835d57145e3ba17be22ff79e47a6abe51b61
Daniel Mitterecker <daniel.mitterecker@zamg.ac.at>
2020-12-04 21:42:52 +0100

    DEV: implemented recursive square root function, separated code in additional file

--- hello_world.rkt
+++ hello_world.rkt
@@ -1,6 +1,25 @@
 #!/usr/bin/env racket
 #lang racket
 
-(define (square x)( * x x))
+(require "mysqrt.rkt")
 
 (square 5)
+
+(define (three_sum a b c) (+ a b c))
+
+(three_sum 1 2 3)
+
+(define (a-plus-abs-b a b)
+    ((if (> b 0) + -) a b))
+
+(a-plus-abs-b 2 -3)
+
+
+(display "average of 10 and 8\n")
+(average 10 8)
+(abs -10)
+
+
+
+; recursive sqrt
+(sqrt 2)
